5eaeb09d9cb2a9416b40eeca24ab39a5ace75548
altriayu <altriayu97@outlook.com>
2024-09-29 17:21:21 +0800

    style fix

--- tests/rs/leetcode_135_test.rs
+++ tests/rs/leetcode_135_test.rs
@@ -1,14 +1,14 @@
 use leetcode::rs::leetcode_135::candy;
 
 #[test]
-fn test_candy_example_1() {
+fn case_1() {
     let input = vec![1, 0, 2];
     let expected_output = 5;
     assert_eq!(candy(input), expected_output);
 }
 
 #[test]
-fn test_candy_example_2() {
+fn case_2() {
     let input = vec![1, 2, 2];
     let expected_output = 4;
     assert_eq!(candy(input), expected_output);
